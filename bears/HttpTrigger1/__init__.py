6628f6b07ec2f9d791f1a955f317f7714442225a
bstee615 <benjaminjsteenhoek@gmail.com>
2021-02-05 14:30:34 -0600

    Integrate predictor with Azure function

--- bears/HttpTrigger1/__init__.py
+++ bears/HttpTrigger1/__init__.py
@@ -1,24 +1,43 @@
 import logging
 
 import azure.functions as func
+from pathlib import Path
+from ai import Predictor
+import requests
+import tempfile
+import json
 
 
+# https://docs.microsoft.com/en-us/azure/azure-functions/machine-learning-pytorch?tabs=bash#update-the-function-to-run-predictions
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    model_path = Path('models/bears.pkl')
+    predictor = Predictor(model_path)
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    image_url = req.params.get('img')
+    if not image_url:
+        logging.error('No image URL in request.')
+        return func.HttpResponse(status_code=400)
+    logging.info(f'Image URL received: {image_url}. Downloading...')
+    r = requests.get(image_url)
+    logging.info(f'Status code {r.status_code}.')
+    
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        tmpdir = Path(tmpdirname)
+        content_file = tmpdir / 'content'
+        logging.info(f'Saving to {content_file}.')
+        with content_file.open('wb') as f:
+            f.write(r.content)
+        results = predictor.predict(content_file)
+        logging.info(results)
+    
+    payload = {
+        "prediction": results[0],
+        "probabilities": results[2].tolist(),
+    }
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    headers = {
+        "Content-type": "application/json",
+        "Access-Control-Allow-Origin": "*"
+    }
+
+    return func.HttpResponse(json.dumps(payload), headers = headers)
